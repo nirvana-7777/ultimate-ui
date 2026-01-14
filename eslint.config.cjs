7539676e661730dd968b21264cab979648ec76fe
Nirvana <Nirvana@nirvana.com>
2026-01-14 21:25:17 +0100

    Stricter Linting

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -20,14 +20,29 @@ module.exports = [
         confirm: "readonly",
         alert: "readonly",
 
-        // Add any other browser-specific globals you use
+        // File and URL APIs
+        Blob: "readonly",
+        URL: "readonly",
+        FileReader: "readonly",
+        File: "readonly",
+
+        // Web APIs
+        IntersectionObserver: "readonly",
+        Intl: "readonly",
+        URLSearchParams: "readonly",
+        Headers: "readonly",
+        Request: "readonly",
+        Response: "readonly",
+        AbortController: "readonly",
+        Event: "readonly",
+        CustomEvent: "readonly",
+
+        // Browser objects
         location: "readonly",
         sessionStorage: "readonly",
         localStorage: "readonly",
         history: "readonly",
         navigator: "readonly",
-        CustomEvent: "readonly",
-        Event: "readonly",
 
         // Your custom globals
         showToast: "readonly",
@@ -35,14 +50,21 @@ module.exports = [
         hideLoading: "readonly",
         epgDisplayManager: "readonly",
         monitoringManager: "readonly",
-        baseTemplate: "readonly"
+        baseTemplate: "readonly",
+        CONFIG_DATA: "readonly", // From config.js
+
+        // EPG related globals
+        EPGCore: "readonly", // Defined in epg_core.js
+        EPGUI: "readonly",   // Defined elsewhere (probably epg_ui.js)
+        EPGPlayer: "readonly" // Defined elsewhere
       }
     },
 
     rules: {
       "no-unused-vars": ["warn", {
         "argsIgnorePattern": "^_",
-        "caughtErrorsIgnorePattern": "^_"
+        "caughtErrorsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_" // Allow unused variables starting with _
       }],
       "no-undef": "error",
       "no-console": "off"
