6c62920221452e26d1f4c4bc7e90455cc23d9023
Nirvana <Nirvana@nirvana.com>
2026-01-14 21:02:17 +0100

    Stricter Linting

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    files: ["src/static/js/**/*.js"],
+    ignores: ["node_modules/**"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "no-console": "off"
+    }
+  }
+];
